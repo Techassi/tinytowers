8afe78c7fd331720b0bd60fc45f627e494545f7e
Techassi <hi@web-warrior.de>
2021-02-13 20:41:46 +0100

    Switch to gridmap class

--- src/gridmap/gridmap.ts
+++ src/gridmap/gridmap.ts
@@ -2,16 +2,17 @@ import 'phaser';
 
 import Cell from './cell';
 import { NumberCouple } from '@/types/generic';
+import { Point } from '@/types/level';
 
-export class GridMap extends Phaser.GameObjects.GameObject {
+export class GridMap extends Phaser.GameObjects.Graphics {
     private static INAVLID: Cell = new Cell('INVALID');
-    private static DEFAULT: Cell = new Cell('DEFAULT');
 
-    private graphic!: Phaser.GameObjects.Graphics;
+    private cellSizeHalf!: number;
     private cellSize!: number;
     private map!: Array<Cell>;
     private height!: number;
     private width!: number;
+    private path!: Phaser.Curves.Path;
 
     public constructor(
         scene: Phaser.Scene,
@@ -19,14 +20,16 @@ export class GridMap extends Phaser.GameObjects.GameObject {
         height: number,
         cellSize: number
     ) {
-        super(scene, 'gridmap');
+        super(scene);
 
+        this.cellSizeHalf = cellSize / 2;
         this.cellSize = cellSize;
         this.height = height;
         this.width = width;
+        this.map = new Array<Cell>();
     }
 
-    public create(): void {
+    addedToScene(): void {
         this.drawGrid();
     }
 
@@ -54,30 +57,73 @@ export class GridMap extends Phaser.GameObjects.GameObject {
         return this.map[x + y * (this.width / this.cellSize)];
     }
 
+    public getPath(): Phaser.Curves.Path {
+        return this.path;
+    }
+
+    public drawPath(path: Point[]): void {
+        this.lineStyle(2, 0x0000ff, 1.0);
+        this.beginPath();
+
+        this.path = new Phaser.Curves.Path(
+            (path[0].x + 1) * this.cellSize - this.cellSizeHalf,
+            (path[0].y + 1) * this.cellSize - this.cellSizeHalf
+        );
+
+        path.forEach((point) => {
+            this.path.lineTo(
+                (point.x + 1) * this.cellSize - this.cellSizeHalf,
+                (point.y + 1) * this.cellSize - this.cellSizeHalf
+            );
+
+            this.gridToCell(point.x, point.y).setType('PATH');
+        });
+
+        this.path.draw(this);
+        this.closePath();
+        this.strokePath();
+    }
+
+    public markPathCells(): void {
+        const side = Math.max(this.width, this.height);
+        const division = Math.max(40, side / this.cellSize);
+        const points = this.path.getSpacedPoints(division);
+
+        points.forEach((point) => {
+            const position = this.worldToGrid(point.x, point.y);
+            const cell = this.gridToCell(position.x, position.y);
+
+            if (cell.getType() == 'INVALID') {
+                return;
+            }
+
+            cell.setType('PATH');
+        });
+    }
+
     private drawGrid(): void {
-        this.graphic = new Phaser.GameObjects.Graphics(this.scene);
-        this.graphic.lineStyle(1, 0xffffff, 1.0);
-        this.graphic.beginPath();
+        this.lineStyle(1, 0xffffff, 1.0);
+        this.beginPath();
 
         for (let x = 0; x < this.width; x += this.cellSize) {
-            this.graphic.moveTo(x, 0);
-            this.graphic.lineTo(x, this.height);
+            this.moveTo(x, 0);
+            this.lineTo(x, this.height);
         }
 
         for (let y = 0; y < this.height; y += this.cellSize) {
-            this.graphic.moveTo(0, y);
-            this.graphic.lineTo(this.width, y);
+            this.moveTo(0, y);
+            this.lineTo(this.width, y);
         }
 
-        const cells =
+        const numberOfCells =
             (this.width / this.cellSize) * (this.height / this.cellSize);
 
-        for (let i = 0; i < cells; i++) {
-            this.map.push(GridMap.DEFAULT);
+        for (let i = 0; i < numberOfCells; i++) {
+            this.map.push(new Cell('DEFAULT'));
         }
 
-        this.graphic.closePath();
-        this.graphic.strokePath();
+        this.closePath();
+        this.strokePath();
     }
 }
 
--- src/level/level.ts
+++ src/level/level.ts
@@ -2,13 +2,12 @@ import 'phaser';
 
 import { CELL_SIZE } from '@/constants/cells';
 import { LevelConfig, Point } from '@/types/level';
+import { GridMap } from '@/gridmap/gridmap';
 
 export class Level extends Phaser.Scene {
-    private pathGraphic!: Phaser.GameObjects.Graphics;
-    private gridGraphic!: Phaser.GameObjects.Graphics;
-    private path!: Phaser.Curves.Path;
     private levelData!: LevelConfig;
-    private gridMap!: Array<Cell>;
+
+    private gridmap!: GridMap;
 
     public constructor(levelData: LevelConfig) {
         super({
@@ -16,7 +15,6 @@ export class Level extends Phaser.Scene {
         });
 
         this.levelData = levelData;
-        this.gridMap = new Array<Cell>();
     }
 
     public init(): void {
@@ -24,124 +22,31 @@ export class Level extends Phaser.Scene {
     }
 
     public create(): void {
-        this.generateGrid();
-        this.generatePath();
-        this.generatePathCellsBasedOnPath();
+        this.gridmap = this.add.existing(
+            new GridMap(
+                this,
+                this.levelData.width,
+                this.levelData.height,
+                CELL_SIZE
+            )
+        );
+
+        this.gridmap.drawPath(this.levelData.path);
+        this.gridmap.markPathCells();
 
         this.input.on('pointerdown', (event: any) => {
-            const position: Phaser.Math.Vector2 = this.worldToGrid(
+            const position: Phaser.Math.Vector2 = this.gridmap.worldToGrid(
                 event.downX,
                 event.downY
             );
-            console.log(this.gridToCell(position.x, position.y).getType());
+            console.log(
+                this.gridmap.gridToCell(position.x, position.y).getType()
+            );
         });
     }
 
     public getLevelConfig(): LevelConfig {
         return this.levelData;
-    }
-
-    private generateGrid(): void {
-        this.gridGraphic = this.add.graphics();
-        this.gridGraphic.lineStyle(1, 0xffffff, 1.0);
-        this.gridGraphic.beginPath();
-
-        for (let x = 0; x < this.levelData.width; x += CELL_SIZE) {
-            this.gridGraphic.moveTo(x, 0);
-            this.gridGraphic.lineTo(x, this.levelData.height);
-        }
-
-        for (let y = 0; y < this.levelData.height; y += CELL_SIZE) {
-            this.gridGraphic.moveTo(0, y);
-            this.gridGraphic.lineTo(this.levelData.width, y);
-        }
-
-        const cells =
-            (this.levelData.width / CELL_SIZE) *
-            (this.levelData.height / CELL_SIZE);
-
-        for (let i = 0; i < cells; i++) {
-            const cell: Cell = new Cell('DEFAULT');
-            this.gridMap.push(cell);
-        }
-
-        this.gridGraphic.closePath();
-        this.gridGraphic.strokePath();
-    }
-
-    // generatePath generates a path based on the provided level data path. If no path is provided, the algorithm falls
-    // back to a random path
-    private generatePath(): void {
-        this.pathGraphic = this.add.graphics();
-        this.pathGraphic.lineStyle(2, 0x0000ff, 1.0);
-        this.pathGraphic.beginPath();
-
-        this.path = new Phaser.Curves.Path(
-            (this.levelData.path[0].x + 1) * CELL_SIZE - CELL_SIZE / 2,
-            (this.levelData.path[0].y + 1) * CELL_SIZE - CELL_SIZE / 2
-        );
-
-        for (let i = 1; i < this.levelData.path.length; i++) {
-            const point = this.levelData.path[i];
-            this.path.lineTo(
-                (point.x + 1) * CELL_SIZE - CELL_SIZE / 2,
-                (point.y + 1) * CELL_SIZE - CELL_SIZE / 2
-            );
-
-            this.gridToCell(point.x, point.y).setType('PATH');
-        }
-
-        this.path.draw(this.pathGraphic);
-        this.pathGraphic.closePath();
-        this.pathGraphic.strokePath();
-    }
-
-    private generatePathCellsBasedOnPath(): void {
-        const side = Math.max(this.levelData.width, this.levelData.height);
-        const division = Math.max(40, side / CELL_SIZE);
-        const points = this.path.getSpacedPoints(division);
-
-        for (let i = 0; i < points.length; i++) {
-            const point = points[i];
-            const position = this.worldToGrid(point.x, point.y);
-            const cell = this.gridToCell(position.x, position.y);
-
-            if (cell.getType() == 'INVALID') {
-                continue;
-            }
-
-            cell.setType('PATH');
-        }
-    }
-
-    // worldToGrid translates the world position to the cell position within the grid map
-    private worldToGrid(x: number, y: number): Phaser.Math.Vector2 {
-        const cellX = Math.floor(x / CELL_SIZE);
-        const cellY = Math.floor(y / CELL_SIZE);
-        return new Phaser.Math.Vector2(cellX, cellY);
-    }
-
-    private gridToCell(x: number, y: number): Cell {
-        if (x < 0 || y < 0) {
-            return new Cell('INVALID');
-        }
-        return this.gridMap[x + y * (this.levelData.width / CELL_SIZE)];
-    }
-}
-
-export class Cell {
-    private type!: string;
-
-    public constructor(type: string) {
-        this.type = type;
-    }
-
-    public setType(type: string): void {
-        this.type = type;
-    }
-
-    public getType(): string {
-        return this.type;
     }
 }
 
